6237c9ba6bfd84b8961c0ce1cbed0455e4782dd2
Giang <trinhgianhsoi@gmail.com>
2025-03-28 08:08:03 +0700

    fix true proxy

--- src/index.ts
+++ src/index.ts
@@ -68,6 +68,10 @@ databaseService.connect().then(() => {
   databaseService.indexTweets();
 });
 const app = express();
+
+// Trust proxy for Vercel
+app.set("trust proxy", 1);
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
